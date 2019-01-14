8d5a13a4c5dbb25c0fff3c6bb1a5d68d8ea65302
Itziar23 <itzirurzun@alumni.unav.es>
2019-01-14 13:26:25 +0100

    vignettes

---
+++ R/ACESO.R
@@ -0,0 +1,14 @@
+#' ACESO
+#' 
+#' ACESO: A Cancer Evolution Simulation Optimizer
+#' 
+#' ACESO is a multiscale computational framework to explore
+#' the evolutionary dynamics of heterogeneous tumor cell populations taking
+#' pharmacokinetic and drug interaction effects into account to rationally identify
+#' optimum single-agent and combination treatment administration strategies for
+#' oncogene-driven cancers.
+#' 
+#' @docType package
+#' @name ACESO
+#' @keywords Cancer Stochastic-models Modeling&Simulation branching-process
+NULL
