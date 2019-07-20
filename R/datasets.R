b3c7b4f0b1bff2e9f00e74cbf40149289b5c1968
SPIDDOR <itzirurzun@alumni.unav.es>
2019-07-20 23:17:27 +0200

    Creating vignettes

--- R/datasets.R
+++ R/datasets.R
@@ -20,7 +20,7 @@ NULL
 
 #' Dactolisib_Trametinib_combination dataset
 #' 
-#' Dataset containing the effect of the combination of Dactolisib and Trametinib on the number of BT-20 cell line cell counts.
+#' Dataset containing the effect of the combination of dactolisib and trametinib on the number of BT-20 cell line cell counts.
 #' @name Dactolisib_Trametinib_combination
 #' @docType data
 #'
@@ -56,9 +56,45 @@ NULL
 #'@keywords datasets
 NULL
 
+#' Alpelisib_Trametinib_combination dataset
+#' 
+#' Dataset containing the effect of the combination of alpelisib and trametinib on the number of BT-20 cell line cell counts.
+#' @name Alpelisib_Trametinib_combination
+#' @docType data
+#'
+#'@usage data(Alpelisib_Trametinib_combination)
+#'
+#'@details
+#'  The data consists of a dataframe named Alpelisib_Trametinib_combination with the following columns:
+#'  \itemize{
+#'   \item Cell.line: name of the cell line used in the experiment. BT-20 in this example.
+#'   \item Drug.Name: Name of one of the drug used in the experiment. Alpelisib in this case.
+#'   \item CONC: concentration of the drug specified in Small.Molecule.Name column.
+#'   \item CONC.units: units of drug concentrations in CONC. Numeric column.
+#'   \item Drug.2.Name: Name of the second drug used in the experiment. Trametinib in this case.
+#'   \item CONC2: concentration of the drug specified in Small.Molecule.2.Name column. Numeric column.
+#'   \item CONC2.units: units of drug concentrations in CONC2.
+#'   \item Replicate: Technical Replicate. Numeric column.
+#'   \item Cell_Count_0: Total cell counts before treatment.
+#'   \item Viable.cells: Total cell count after treatment
+#'   \item Control: Total control cell count.
+#'   \item Time: Time where the viable cells are counted after treatment.
+#'   \item Type: drug sensitive cell type. Number 0 in this case.
+#'   }
+#'
+#'  
+#' 
+#'@examples
+#'\dontrun{
+#'  data(Alpelisib_Trametinib_combination)
+#'  print(Alpelisib_Trametinib_combination)
+#'}
+#'@keywords datasets
+NULL
+
 #' Dactolisib_Trametinib_rates dataset
 #' 
-#' Dataset containing the effect of the combination of Dactolisib and Trametinib on the birth rate of drug sensitive BT-20 cell line.
+#' Dataset containing the effect of the combination of dactolisib and trametinib on the birth rate of drug sensitive BT-20 cell line.
 #' @name GD
 #' @docType data
 #'
@@ -82,6 +118,68 @@ NULL
 #'\dontrun{
 #'  data(Dactolisib_Trametinib_rates)
 #'  head(GD)
+#'}
+#'@keywords datasets
+NULL
+
+#' Alpelisib_sensitive dataset
+#' 
+#' Dataset containing the effect of alpelisib on the birth and death rates of alpelisib-sensitive BT-20 cell line (example data adapted from the effect of alpelisib on the BT-20 cell line data obtained from the HMS LINCS database)..
+#' @name Alpelisib_sensitive
+#' @docType data
+#'
+#'@usage data(Alpelisib_sensitive)
+#'
+#'@details
+#'  The data consists of a dataframe named Alpelisib_sensitive with the following columns:
+#'  \itemize{
+#'   \item Cell.line: name of the cell line used in the experiment. BT-20 in this example.
+#'   \item Time: time point (hours).
+#'   \item CONC: concentration of the drug 1. Numeric column.
+#'   \item Type: drug resistant cell type. Number 1 in this case.
+#'   \item Net_growth: net growth rates of the cells under different drug concentrations.
+#'   \item Birth_rate: birth rate of the sensitive cell line influenced by different drug concentrations.
+#'   \item Death_rate: Constant death rate.
+#' }
+#'
+#'  
+#' 
+#'@examples
+#'\dontrun{
+#'  data(Alpelisib_sensitive)
+#'  head(Alpelisib_sensitive)
+#'}
+#'@keywords datasets
+NULL
+
+
+#' Trametinib_sensitive dataset
+#' 
+#' Dataset containing the effect of Trametinib on the birth and death rates of trametinib-sensitive BT-20 cell line 
+#' (example data adapted from the effect of trametinib on the BT-20 cell line data obtained from the HMS LINCS database).
+#' @name Trametinib_sensitive
+#' @docType data
+#'
+#'@usage data(Trametinib_sensitive)
+#'
+#'@details
+#'  The data consists of a dataframe named Trametinib_sensitive with the following columns:
+#'  \itemize{
+#'   \item Cell.line: name of the cell line used in the experiment. BT-20 in this example.
+#'   \item Time: time point (hours).
+#'   \item CONC: concentration of the drug 1. Numeric column.
+#'   \item Type: drug resistant cell type. Number 2 in this case.
+#'   \item Net_growth: net growth rates of the cells under different drug concentrations.
+#'   \item Birth_rate: birth rate of the sensitive cell line influenced by different drug concentrations.
+#'   \item Death_rate: Constant death rate.
+#' }
+#'
+#'  
+#' 
+#'@examples
+#'\dontrun{
+#'  data(Trametinib_sensitive)
+#'  head(Trametinib_sensitive)
 #'}
 #'@keywords datasets
 NULL
